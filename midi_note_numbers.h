78ab035a19bc82a5f9c3d7fe933767cb044936c8
doug makofka <doug@makofka.org>
2021-08-06 10:51:39 -0400

    Add files via upload

---
+++ midi_note_numbers.h
@@ -0,0 +1,66 @@
+
+
+#define NC1   36
+#define NCS1  37
+#define ND1   38
+#define NDS1  39
+#define NE1   40
+#define NF1   41
+#define NFS1  42
+#define NG1   43
+#define NGS1  44
+#define NA1   45
+#define NAS1  46
+#define NB2   47
+
+#define NC2   48
+#define NCS2  49
+#define ND2   50
+#define NDS2  51
+#define NE2   52
+#define NF2   53
+#define NFS2  54
+#define NG2   55
+#define NGS2  56
+#define NA2   57
+#define NAS2  58
+#define NB2   59
+
+#define NC3   60
+#define NCS3  61
+#define ND3   62
+#define NDS3  63
+#define NE3   64
+#define NF3   65
+#define NFS3  66
+#define NG3   67
+#define NGS3  68
+#define NA3   69
+#define NAS3  70
+#define NB3   71
+
+#define NC4   72
+#define NCS4  73
+#define ND4   74
+#define NDS4  75
+#define NE4   76
+#define NF4   77
+#define NFS4  78
+#define NG4   79
+#define NGS4  80
+#define NA4   81
+#define NAS4  82
+#define NB4   83
+
+#define NC4   60
+#define NCS4  61
+#define ND4   62
+#define NDS4  63
+#define NE4   64
+#define NF4   65
+#define NFS4  66
+#define NG4   67
+#define NGS4  68
+#define NA4   69
+#define NAS4  70
+#define NB4   71
